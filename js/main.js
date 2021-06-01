2aa217fc127c7b5d60cdcedf2738197827c89c3c
iwtts <berzinao@hotmail.com>
2021-06-01 12:52:19 +0300

    Добавил функцию, возвращающую случайное целое число из переданного диапазона включительно

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+//За основу взял функцию getRandomIntInclusive из mdn https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function getRandomIntInclusive(from, to) {
+  if (to > from && from >= 0) {
+    from = Math.ceil(from);
+    to = Math.floor(to);
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+  }
+  if (from > to && to >= 0) {
+    to = Math.ceil(to);
+    from = Math.floor(from);
+    return Math.floor(Math.random() * (from - to + 1)) + to;
+  }
+  if (from === to) {
+    return from;
+  }
+  return 'Ошибка. Диапазон не может включать отрицательные числа.';
+}
+
+getRandomIntInclusive();
